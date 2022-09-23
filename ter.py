7303a5082fdab96f1cf910e3160ff4571ad4bb05
mcspidy <mcspidy@comcast.net>
2022-09-23 16:01:24 -0500

    Added All Requirements

---
+++ ter.py
@@ -0,0 +1,4 @@
+a={1:"one"}
+def ter_m():
+    print("added to path")
+
---
+++ ty.py
@@ -0,0 +1,10 @@
+
+class tdic:
+    data = {"ss": 123,"cc":45}
+    def type(self):
+        return self.data["s"] if "s" in data else 'one'
+
+
+c=tdic()
+for x,y in c.data.items():
+    print(x,y)
